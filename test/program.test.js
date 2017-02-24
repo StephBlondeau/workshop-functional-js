c2c9b815372abeff764a4f0721d2de735c8d92e0
Stéphanie Blondeau <stephanie.blondeau@laposte.net>
2017-02-24 09:17:31 +0100

    Exercice 1

--- test/program.test.js
+++ test/program.test.js
@@ -1,12 +1,32 @@
 var expect = require('chai').expect;
 
 var transformCheckpoint = require('../src/program').transformCheckpoint;
-
+var Point = {
+  id: 'whataw0nd3rful1d',
+  uuid: 'whataw0nd3rful1d',
+  address: 'unknown',
+  addressType: 'unknown',
+  connectable: true,
+  advertisement: {
+    localName: undefined,
+    txPowerLevel: undefined,
+    manufacturerData: undefined,
+    serviceData: [],
+    serviceUuids: [ 'abcd' ]
+  },
+  rssi: -66,
+  services: null,
+  state: 'outofcontrol'
+}
 
 describe('Function transformCheckpoint', function() {
 
   it('Function transformCheckpoint without parameter should return false', function() {
     expect(transformCheckpoint()).to.be.false;
+  });
+
+  it('Function transformCheckpoint without parameter should verify if parameter is mutated', function() {
+    expect(transformCheckpoint(Point)).to.eql(Point);
   });
 
 });
